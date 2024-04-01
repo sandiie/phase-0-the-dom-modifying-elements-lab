9f8220d684175ba3e3e71fc8e255359fd92fd145
+ <+>
2024-04-01 18:47:28 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const element = document.getElementById("main");  
+element.remove();
+
+
+const newHeader = document.createElement("h1");
+ newHeader.setAttribute("id","victory");
+
+
+newHeader.innerHTML = "YOUR - NAME is the champion";
